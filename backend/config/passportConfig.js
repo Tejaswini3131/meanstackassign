cbf350808e2df309067f04e5635bb0b9ce01f4a5
Tejaswini3131 <64706681+Tejaswini3131@users.noreply.github.com>
2021-05-10 02:48:11 +0530

    Update passportConfig.js

--- backend/config/passportConfig.js
+++ backend/config/passportConfig.js
@@ -10,13 +10,13 @@ passport.use(
                 (err, user) => {
                     if (err)
                         return done(err);
-                    // unknown user
+                    
                     else if (!user)
                         return done(null, false, { message: 'Email is not registered' });
-                    // wrong password
+                   
                     else if (!user.verifyPassword(password))
                         return done(null, false, { message: 'Wrong password.' });
-                    // authentication succeeded
+                    
                     else
                         return done(null, user);
                 });
